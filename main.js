6fc151615ebbd22e30e7b15c7a84852fb76c747e
José Antonio Sánchez Ortiz <jasofree@gmail.com>
2022-03-09 11:33:49 +0100

    Scroller for world map

--- main.js
+++ main.js
@@ -4,51 +4,53 @@ let contentWrapperNode = document.querySelector('#content_wrapper');
 let minimapWrapperNode = document.querySelector('#minimap_wrapper');
 let minimapContentWrapperNode = document.querySelector('#minimap_content_wrapper');
 let minimapViewportNode = document.querySelector('#minimap_viewport');
-let ratioHorizontalMinimapContent, ratioVerticalMinimapContent;
+let minimapWrapperBoundingClientRect, ratioHorizontalContent, ratioVerticalContent, ratioHorizontalMinimapContent, ratioVerticalMinimapContent;
 
 window.addEventListener('load', () => {
-  fetch("./assets/image/chile.svg")
+  fetch("./assets/image/world.svg")
     .then(response => response.text())
     .then((svg) => {
       contentWrapperNode.insertAdjacentHTML("afterbegin", svg);
       minimapContentWrapperNode.insertAdjacentHTML("afterbegin", svg);
-      let contentWrapperSvgNode = contentWrapperNode.querySelector('svg');
-      const viewBoxValue = `0 0 ${parseInt(contentWrapperSvgNode.getAttribute('width'))} ${parseInt(contentWrapperSvgNode.getAttribute('height'))}`;
-      contentWrapperSvgNode.setAttribute('viewBox', viewBoxValue);
-      const contentWrapperComputedStyle = window.getComputedStyle(contentWrapperNode);
-      const minimapContentWrapperComputedStyle = window.getComputedStyle(minimapContentWrapperNode);
-      const contentWrapperNodeWidth = contentWrapperNode.clientWidth;
-      const minimapContentWrapperNodeWidth = minimapContentWrapperNode.clientWidth;
-      const contentWidth = contentWrapperNodeWidth - parseInt(contentWrapperComputedStyle['padding-left']) - parseInt(contentWrapperComputedStyle['padding-right']);
-      const minimapContentWidth = minimapContentWrapperNodeWidth - parseInt(minimapContentWrapperComputedStyle['padding-left']) - parseInt(minimapContentWrapperComputedStyle['padding-right']);
-      ratioHorizontalMinimapContent = minimapContentWidth / contentWidth;
-      handle_content_scroll();
+      minimapWrapperBoundingClientRect = minimapWrapperNode.getBoundingClientRect();
+      let contentWrapperBoundingClientRect = contentWrapperNode.getBoundingClientRect();
+      let minimapContentWrapperBoundingClientRect = minimapContentWrapperNode.getBoundingClientRect();
+      let windowViewportWidth = window.innerWidth;
+      let windowViewportHeight = window.innerHeight;
+      ratioHorizontalContent = windowViewportWidth / contentWrapperBoundingClientRect.width;
+      ratioVerticalContent = windowViewportHeight / contentWrapperBoundingClientRect.height;
+      ratioHorizontalMinimapContent = minimapContentWrapperBoundingClientRect.width / contentWrapperBoundingClientRect.width;
+      ratioVerticalMinimapContent = minimapContentWrapperBoundingClientRect.height / contentWrapperBoundingClientRect.height;
+      minimapViewportNode.style.width = `${100 * ratioHorizontalContent}%`;
+      minimapViewportNode.style.height = `${100 * ratioVerticalContent}%`;
+      minimapViewportNode.style.display = 'block';
+      update_minimap_viewport_position();
+      contentWrapperNode.querySelectorAll('#content_wrapper svg path[id], #content_wrapper svg path[class], #content_wrapper svg path[name]').forEach((element) => {
+        element.addEventListener('mouseenter', (event) => {
+          element.classList.add('highlighted');
+        });
+        element.addEventListener('mouseleave', (event) => {
+          element.classList.remove('highlighted');
+        });
+      });
     });
 });
 
 let handling_content_scroll = false;
-let handle_content_scroll = function() {
-  let windowViewportHeight = window.innerHeight;
-  const contentWrapperNodeHeight = contentWrapperNode.clientHeight;
-  const minimapContentWrapperNodeHeight = minimapContentWrapperNode.clientHeight;
-  ratioVerticalMinimapContent = minimapContentWrapperNodeHeight / contentWrapperNodeHeight;
-  minimapViewportNode.style.display = 'block';
-  minimapViewportNode.style.height = `${windowViewportHeight * ratioVerticalMinimapContent}px`;
+let update_minimap_viewport_position = function() {
   let contentWrapperNodeBoundingClient = contentWrapperNode.getBoundingClientRect();
   let contentWrapperNodeTop = contentWrapperNodeBoundingClient.top;
+  let contentWrapperNodeLeft = contentWrapperNodeBoundingClient.left;
   let newMinimapViewportNodeTop = Math.round(contentWrapperNodeTop * ratioVerticalMinimapContent);
-  let minimapWrapperHeight = minimapWrapperNode.clientHeight;
-  if (minimapWrapperHeight > windowViewportHeight) {
-    let topScrolledPercentage = contentWrapperNodeTop / (contentWrapperNodeHeight - windowViewportHeight);
-    minimapWrapperNode.style.top = `${Math.round(topScrolledPercentage * (minimapWrapperHeight - windowViewportHeight))}px`;
-  }
+  let newMinimapViewportNodeLeft = Math.round(contentWrapperNodeLeft * ratioHorizontalMinimapContent);
   minimapViewportNode.style.top = `${-1 * newMinimapViewportNodeTop}px`;
+  minimapViewportNode.style.left = `${-1 * newMinimapViewportNodeLeft}px`;
 };
 
 window.addEventListener('scroll', () => {
   if (!handling_content_scroll) {
       window.requestAnimationFrame(() => {
-        handle_content_scroll();
+        update_minimap_viewport_position();
         handling_content_scroll = false;
       });
   }
@@ -56,19 +58,80 @@ window.addEventListener('scroll', () => {
 });
 
 let handling_minimap_wheel = false;
-let handle_minimap_wheel = function(deltaY) {
-  document.querySelector('html').scrollTop = document.querySelector('html').scrollTop + Math.round(deltaY);
-  handle_content_scroll();
-};
-
 minimapContentWrapperNode.addEventListener('wheel', (event) => {
   event.stopPropagation();
   event.preventDefault();
   if (!handling_minimap_wheel) {
       window.requestAnimationFrame(() => {
-        handle_minimap_wheel(event.deltaY);
-          handling_minimap_wheel = false;
+        update_minimap_viewport_position();
+        handling_minimap_wheel = false;
       });
   }
   handling_minimap_wheel = true;
 });
+
+let handling_minimap_viewport_drag = false;
+let draggingMinimapViewportInfo = undefined;
+minimapViewportNode.addEventListener('mousedown', (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+  let minimapViewportBoundingClientRect = minimapViewportNode.getBoundingClientRect();
+  draggingMinimapViewportInfo = {
+    minimap: {
+      x: minimapWrapperBoundingClientRect.x,
+      y: minimapWrapperBoundingClientRect.y,
+      height: minimapWrapperBoundingClientRect.height,
+      width: minimapWrapperBoundingClientRect.width
+    },
+    minimapViewport: {
+      height: minimapViewportBoundingClientRect.height,
+      width: minimapViewportBoundingClientRect.width,
+      origin: {
+        x: minimapViewportBoundingClientRect.x,
+        y: minimapViewportBoundingClientRect.y
+      },
+      draggingOrigin: {
+        x: event.x,
+        y: event.y
+      }
+    }
+  }
+});
+
+minimapWrapperNode.addEventListener('mousemove', (event) => {
+  if (!handling_minimap_viewport_drag) {
+    window.requestAnimationFrame(() => {
+      if (draggingMinimapViewportInfo) {
+        let newMinimapViewportNodeTop = draggingMinimapViewportInfo.minimapViewport.origin.y - draggingMinimapViewportInfo.minimap.y + event.y - draggingMinimapViewportInfo.minimapViewport.draggingOrigin.y;
+        let newMinimapViewportNodeLeft = draggingMinimapViewportInfo.minimapViewport.origin.x - draggingMinimapViewportInfo.minimap.x + event.x - draggingMinimapViewportInfo.minimapViewport.draggingOrigin.x;
+        if (newMinimapViewportNodeTop < 0) {
+          newMinimapViewportNodeTop = 0;
+        }
+        if (newMinimapViewportNodeTop > draggingMinimapViewportInfo.minimap.height - draggingMinimapViewportInfo.minimapViewport.height) {
+          newMinimapViewportNodeTop = draggingMinimapViewportInfo.minimap.height - draggingMinimapViewportInfo.minimapViewport.height;
+        }
+        if (newMinimapViewportNodeLeft < 0) {
+          newMinimapViewportNodeLeft = 0;
+        }
+        if (newMinimapViewportNodeLeft > draggingMinimapViewportInfo.minimap.width - draggingMinimapViewportInfo.minimapViewport.width) {
+          newMinimapViewportNodeLeft = draggingMinimapViewportInfo.minimap.width - draggingMinimapViewportInfo.minimapViewport.width;
+        }
+        let newContentWrapperNodeTop = Math.round(newMinimapViewportNodeTop / ratioVerticalMinimapContent);
+        let newContentWrapperNodeLeft = Math.round(newMinimapViewportNodeLeft / ratioHorizontalMinimapContent);
+        minimapViewportNode.style.top = `${newMinimapViewportNodeTop}px`;
+        minimapViewportNode.style.left = `${newMinimapViewportNodeLeft}px`;
+        document.querySelector('html').scrollTop = newContentWrapperNodeTop;
+        document.querySelector('html').scrollLeft = newContentWrapperNodeLeft;
+      }
+      handling_minimap_viewport_drag = false;
+    });
+  }
+  handling_minimap_viewport_drag = true;
+});
+
+minimapWrapperNode.addEventListener('mouseup', (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+  draggingMinimapViewportInfo = undefined;
+});
+
